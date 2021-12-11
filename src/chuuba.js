14c1b367b071405b3a7a5bb49505cf793e09806f
haaaaruuuu <mccodi11@gmail.com>
2021-12-11 01:53:52 -0800

    class creation

--- src/chuuba.js
+++ src/chuuba.js
@@ -1,5 +1,3 @@
-const { memberNicknameMention } = require('@discordjs/builders');
-
 class Vtuber {
     constructor(
         name, name_jp, generation, youtube, bilibili, twitter, official,
@@ -17,32 +15,69 @@ class Vtuber {
         this.hashtag = hashtag;
         this.emoji = emoji;
 
-        if (!folder.initialStart()) {
+        if (!folder.entryCreated(this.youtube)) {
             this.createEntry();
         }
-    }
+    };
+
+    async 
 
     async update(){
+
         let data = {
-            {
-                ''
+            'twitter': {
+                'link': '',
+                'msg_id': ''
+            },
+            'livestream': {
+                'live': false,
+                'link': '',
+                'msg_id': ''
+            },
+            'stats': {
+                'twitter': {
+                    'current': '',
+                    'prev': ''
+                },
+                'youtube': {
+                    'current': '',
+                    'prev': ''
+                }
             }
         }
         
         let path = '../../mem/' + this.youtube + '.json';
         const fs = require('fs');
-        fs.writeFile(path, data, function (err){
-            console.log('error');
-        }
-        );
-
-
-    }
+        let json = JSON.stringify(data);
+        fs.writeFile(path, json, function (err){
+            console.log(err);
+        });
+    };
 
     createEntry() {
-        let path = '../../mem/' + this.youtube + '.json';
+        let dir = './mem/';
+        let path = dir + this.youtube + '.json';
 
         const fs = require('fs');
-        fs.createWriteStream(path);
-    }
+
+        fs.mkdir(dir, {recursive: true}, (err) => {
+            if (err) {
+                return console.error(err);
+            }
+            console.log("created");
+        })
+        let data = "meme";
+        fs.writeFile(path, data, { flag: 'wx' }, function (err) {
+            if (err) throw err;
+        console.log("It's saved!");
+        });
+    };
 }
+
+let peko = new Vtuber("meme", "meme", "meme","123123");
+
+peko.update()
+    .then()
+    .catch((error) => {
+        console.log("meme");
+    });
